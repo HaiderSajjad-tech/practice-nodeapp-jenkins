f002e423833f0b380742e7d4ea27590c305d9848
HaiderSajjad-tech <85832044+HaiderSajjad-tech@users.noreply.github.com>
2022-06-20 00:14:41 +0500

    Update node-js-example.js

--- node-js-example.js
+++ node-js-example.js
@@ -5,6 +5,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     // write the response and send it to the client
     res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write(' NO Succes');
+    res.write('Deployment Succeeded');
     res.end();
 }).listen(9051);
